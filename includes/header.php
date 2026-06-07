748ee60f4baecc6df6ded8925adab8dc0d9bdfb7
MD. ERFAN AKIN NISHAB <84656099+Nishab01@users.noreply.github.com>
2026-06-07 16:47:27 +0600

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
     <!-- MOBILE VIEW -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <meta name="google-site-verification" content="" /> 
+    <meta name="google-site-verification" content=""/> 
 
     <title><?= isset($pagename) ? $pagename : 'PureCinepix' ?></title>
 
