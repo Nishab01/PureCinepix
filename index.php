ff08c3b33f12af1b16370e190a2bcd77ac64ffd7
MD. ERFAN AKIN NISHAB <84656099+Nishab01@users.noreply.github.com>
2026-06-07 16:45:49 +0600

    Change Google site verification content to empty
    Updated Google site verification meta tag to empty.

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="google-site-verification" content="cw18mpWd3hibuLyUpAIszw1BHE9wbtoEDqHuPbmg23Q" />
+    <meta name="google-site-verification" content="" /> 
+    
     <title>PureCinepix</title>
 
     <link rel="icon" href="./assets/favicon-32.ico">
