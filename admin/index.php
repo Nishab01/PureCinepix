12268029947e2127fa931a8edbd3eee9f725453b
MD. ERFAN AKIN NISHAB <84656099+Nishab01@users.noreply.github.com>
2026-06-07 16:46:53 +0600

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,6 @@
 <?php
-require_once '../config/config.php';
-require_once '../config/constants.php';
+// require_once '../config/config.php';
+// require_once '../config/constants.php';
 
 // Redirect to actual homepage
 header("Location: ./pages/index.php");
