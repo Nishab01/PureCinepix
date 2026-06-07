c80bb904a236913ee4d132ba19318ade1830eabe
MD. ERFAN AKIN NISHAB <84656099+Nishab01@users.noreply.github.com>
2026-06-07 16:47:53 +0600

    Update header.php

--- admin/partials/header.php
+++ admin/partials/header.php
@@ -11,7 +11,7 @@ if (session_status() === PHP_SESSION_NONE) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <meta name="google-site-verification" content="cw18mpWd3hibuLyUpAIszw1BHE9wbtoEDqHuPbmg23Q" />
+    <meta name="google-site-verification" content=""/>
 
     <title><?= $page_title ?></title>
 
